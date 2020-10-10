b60db8a165d57613d506c2c1dc2e9289fdfb1822
b7s9 <interactive@b7s9.com>
2020-10-10 13:02:31 -0400

    ♻️ Cleanup

--- static/js/formhandler.js
+++ static/js/formhandler.js
@@ -1,39 +1,29 @@
+// --------------------------------------------
+// VARIABLES
+// --------------------------------------------
 const form = document.getElementById("new-entry");
 const dateField = document.getElementById('todo-date');
 
-let date = new Date();
-date.setDate(date.getDate() + 7);
-const dateValue = `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}`
-dateField.value = dateValue;
+const getWeekFromToday = () => {
+	let date = new Date();
+	// default to a week from today
+	date.setDate(date.getDate() + 7);
+	// format human readable
+	const dateValue = `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}`
+	return dateValue;
+}
 
-// form.addEventListener("submit", (e) => {
-//   e.preventDefault();
-//   let formdata = new FormData(form);
-//   // for (var value of formData.entries()) {
-//   //   console.log(value);
-//   // }
-//   // console.log(formdata.getAll())
+// set the html value (FF/Chrome override this)
+dateField.value = getWeekFromToday();
 
-//   let request = new XMLHttpRequest();
-//   request.open("POST", "/add/nice");
-//   request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded')
-//   request.send(formdata);
-//   console.log(request)
-// });
+// --------------------------------------------
+// EVENT HANDLERS
+// --------------------------------------------
 
-// form.addEventListener("formdata", (e) => {
-//   console.log("formdata fired");
-
-//   let data = {};
-//   e.formData.forEach((value, key) => { data[key] = value });
-//   let json = JSON.stringify(data);
-
-//   console.log(json)
-
-//   // submit the data via XHR
-//   let request = new XMLHttpRequest();
-//   request.open("POST", "/post");
-//   request.setRequestHeader('Content-Type', 'application/json')
-//   request.send(e.formData);
-//   console.log(request)
-// });
+form.addEventListener('submit', (e) => {
+	e.preventDefault()
+	let data = new FormData(form)
+	for (entry of data.entries()) {
+		console.log(entry[0] + ', ' + entry[1])
+	}
+})
