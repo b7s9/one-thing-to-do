ec72133d19610e4a1c8f96b8c7212a834b92def7
b7s9 <interactive@b7s9.com>
2020-11-11 18:51:13 -0500

    ✨ Add full manifest with icons

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,9 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
-const { GenerateSW, InjectManifest } = require('workbox-webpack-plugin');
+const { GenerateSW } = require('workbox-webpack-plugin');
+const WebpackPwaManifest = require('webpack-pwa-manifest')
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
 	devServer: {
@@ -16,8 +18,9 @@ module.exports = {
 		add: ['./src/js/add.js']
 	},
 	output: {
-		filename: 'assets/js/[name].[contenthash].js',
+		filename: 'js/[name].[contenthash].js',
 		path: path.resolve(__dirname, 'dist'),
+		publicPath: '/'
 	},
 	module: {
 		rules: [
@@ -29,10 +32,18 @@ module.exports = {
 				],
 			},
 			{
-				test: /\.(png|svg|jpg|gif|ico|woff|woff2|eot|ttf|otf)$/,
-				use: [
-					'file-loader',
-				],
+				test: /\.(png|svg|jpg|gif)$/,
+				loader: 'file-loader',
+				options: {
+					name: 'images/[name].[contenthash].[ext]'
+				}
+			},
+			{
+				test: /\.(woff|woff2|eot|ttf|otf)$/,
+				loader: 'file-loader',
+				options: {
+					name: 'fonts/[name].[contenthash].[ext]'
+				}
 			},
 		],
 	},
@@ -47,6 +58,33 @@ module.exports = {
 			chunks: ['index'],
 			filename: 'index.html',
 			template: path.join(__dirname, 'src/screens', 'index.html')
+		}),
+		new CopyPlugin({
+			patterns: [
+				{ from: 'static', to: 'static' }
+			]
+		}),
+		new WebpackPwaManifest({
+			name: 'One Thing to Do',
+			short_name: 'One Thing',
+			description: 'Remove anxiety from your todo list',
+			background_color: '#ffffff',
+			theme_color: '#ffffff',
+			display: 'standalone',
+			crossorigin: 'use-credentials', //can be null, use-credentials or anonymous
+			orientation: 'portrait-primary',
+			icons: [
+				{
+					src: path.join(__dirname, 'src/style/icons/', 'android-chrome-192x192.png'),
+					destination: 'icons/',
+					sizes: [96, 128, 192] // multiple sizes
+				},
+				{
+					src: path.join(__dirname, 'src/style/icons/', 'android-chrome-512x512.png'),
+					destination: 'icons/',
+					sizes: [256, 384, 512] // multiple sizes
+				}
+			]
 		}),
 		new GenerateSW({
 			cacheId: 'one-thing-to-do',
